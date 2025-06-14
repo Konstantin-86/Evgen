9907e1b7f0235e28a0be3ea41086b26b5fe0f03b
Konstantin <yakimtsev.konstantin@gmail.com>
2025-06-14 12:58:50 +0500

    try github homePage 5

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 
+const basename = window.location.pathname.startsWith('/Evgen') ? '/Evgen' : '/';
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -29,7 +31,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Router >
-        <Footer basename={import.meta.env.BASE_URL} />
+        <Footer basename={basename} />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/stat" element={<Statictics />} />
